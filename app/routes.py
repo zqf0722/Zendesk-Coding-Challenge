8ca84b344ee76539d681404fd8ae84d2c05195a1
zqf0722 <zqf0722@mail.ustc.edu.cn>
2021-11-20 18:39:32 -0500

    pagination

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import EmptyForm
 import requests
@@ -8,23 +8,19 @@ from requests.auth import HTTPBasicAuth
 @app.route('/')
 @app.route('/index', methods = ['GET','POST'])
 def index():
-    tickets = [
-        {
-            "subject": "Help, my printer is on fire!",
-            "requester_id": '20978392',
-            "created_at": "2009-07-20T22:55:29Z"
-        },
-        {
-            "subject": "Testing tickets for fun",
-            "requester_id": '7777777',
-            "created_at": "2021-11-19T23:20:29Z"
-        }
-    ]
-    return render_template('index.html', title='Home', tickets=tickets)
+    return render_template('index.html', title='Home')
 
 
 @app.route('/gettickets')
 def gettickets():
-    url = app.config['SUB_DOMAIN']+'api/v2/tickets'
+    defaulturl = app.config['SUB_DOMAIN'] + 'api/v2/tickets.json?page[size]=25'
+    url = request.args.get('url', defaulturl, type=str)
+    flash(url)
     r = requests.get(url, auth=HTTPBasicAuth(app.config['EMAIL_ADDRESS'], app.config['API_TOKEN']))
-    return r.json()
+    tickets = r.json()['tickets']
+    prevurl = url_for('gettickets', url=r.json()['links']['prev']) if url != defaulturl else None
+    nexturl = url_for('gettickets', url=r.json()['links']['next']) if r.json()['meta']['has_more'] else None
+    flash(prevurl)
+    flash(nexturl)
+    flash(url == defaulturl)
+    return render_template('tickets.html', tickets=tickets, prev_url=prevurl, next_url=nexturl)
