686050f80a8d3bbe01ff5d0f04707823bb6976af
zqf0722 <zqf0722@mail.ustc.edu.cn>
2021-11-20 21:51:32 -0500

    Search for a specific request with ID, facelift of the User Interface

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+from flask import request
+
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
+
+
+class SearchForm(FlaskForm):
+    id = StringField('Request for a ticket with ID', validators=[DataRequired()])
+    submit = SubmitField('Go')
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,44 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, g, flash
 from app import app
-from app.forms import EmptyForm
+from app.forms import SearchForm
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-@app.route('/')
-@app.route('/index', methods = ['GET','POST'])
+@app.before_request
+def before_request():
+    g.search_form = SearchForm()
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', title='Home')
+    url = app.config['SUB_DOMAIN']+'api/v2/tickets/count'
+    r = requests.get(url, auth=HTTPBasicAuth(app.config['EMAIL_ADDRESS'], app.config['API_TOKEN']))
+    num = r.json()['count']['value']
+    return render_template('index.html', count=num)
 
 
 @app.route('/gettickets')
 def gettickets():
     defaulturl = app.config['SUB_DOMAIN'] + 'api/v2/tickets.json?page[size]=25'
     url = request.args.get('url', defaulturl, type=str)
-    flash(url)
     r = requests.get(url, auth=HTTPBasicAuth(app.config['EMAIL_ADDRESS'], app.config['API_TOKEN']))
     tickets = r.json()['tickets']
     prevurl = url_for('gettickets', url=r.json()['links']['prev']) if url != defaulturl else None
     nexturl = url_for('gettickets', url=r.json()['links']['next']) if r.json()['meta']['has_more'] else None
-    flash(prevurl)
-    flash(nexturl)
-    flash(url == defaulturl)
     return render_template('tickets.html', tickets=tickets, prev_url=prevurl, next_url=nexturl)
+
+
+@app.route('/search')
+def search():
+    ticket_id = str(g.search_form.id.data)
+    url = app.config['SUB_DOMAIN']+'api/v2/tickets/'+ticket_id
+    r = requests.get(url, auth=HTTPBasicAuth(app.config['EMAIL_ADDRESS'], app.config['API_TOKEN']))
+    if 'ticket' in r.json():
+        ticket = r.json()['ticket']
+        return render_template('ticketdetail.html', ticket=ticket)
+    else:
+        flash('Error, please input a valid id of a ticket')
+        return redirect(url_for('index'))
+
